dd8fe8b91983ddc309e9d1f0cf77d9b9b5ebfe27
莫锹文 <183795044@qq.com>
2018-08-01 11:21:21 +0800

    UIView常用扩展

--- SwiftLiteKit/Classes/Class/UIView+Extension.swift
+++ SwiftLiteKit/Classes/Class/UIView+Extension.swift
@@ -6,7 +6,7 @@
 //
 import UIKit
 
-//extension UIView: SwiftLiteKitCompatible {}
+// extension UIView: SwiftLiteKitCompatible {}
 
 extension SwiftLiteKit where Base: UIView
 {
@@ -31,7 +31,6 @@ extension SwiftLiteKit where Base: UIView
         view.subviews.forEach { printView(view: $0) }
     }
 
-    
     /// 设置圆角，注意这种设圆角的方法会导致离屏渲染，不要大量使用
     public func setCornerRadius(value: CGFloat, borderWidth: CGFloat, borderColor: UIColor, backgroundColor: UIColor? = nil)
     {
@@ -42,5 +41,50 @@ extension SwiftLiteKit where Base: UIView
 
         guard let color = backgroundColor else { return }
         self.base.backgroundColor = color
+    }
+}
+
+extension UIView
+{
+    public var x: CGFloat
+    {
+        get { return self.frame.origin.x }
+        set { self.frame.origin.x = newValue }
+    }
+
+    public var y: CGFloat
+    {
+        get { return self.frame.origin.y }
+        set { self.frame.origin.y = newValue }
+    }
+
+    public var width: CGFloat
+    {
+        get { return self.frame.size.width }
+        set { self.frame.size.width = newValue }
+    }
+
+    public var height: CGFloat
+    {
+        get { return self.frame.size.height }
+        set { self.frame.size.height = newValue }
+    }
+
+    public var centerX: CGFloat
+    {
+        get { return self.center.x }
+        set { self.center.x = newValue }
+    }
+
+    public var centerY: CGFloat
+    {
+        get { return self.center.y }
+        set { self.center.y = newValue }
+    }
+
+    public var size: CGSize
+    {
+        get { return self.frame.size }
+        set { self.frame.size = newValue }
     }
 }
