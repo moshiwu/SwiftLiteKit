3a47730b4fd2968c5da9f101c60724c95b0410cf
莫锹文 <183795044@qq.com>
2019-02-25 10:08:32 +0800

    fix bug of associate object

--- SwiftLiteKit/Classes/Class/NSObject+Extension.swift
+++ SwiftLiteKit/Classes/Class/NSObject+Extension.swift
@@ -7,25 +7,29 @@
 
 extension NSObject: SwiftLiteKitCompatible {}
 
-extension SwiftLiteKit where Base : NSObject
-{
-    public func getAssociate(with key: String) -> Any?
-    {
-        return objc_getAssociatedObject(self.base, key)
+extension SwiftLiteKit where Base: NSObject {
+    // key要求的是指针，String在Swift作为结构体，如果直接使用虽然不会报错，但自动转成的指针地址会改变，因此要额外处理
+    // let String可以用unsafeMutableRawPointer()
+    // var String直接用&value即可
+
+    public func getAssociate(with key: String) -> Any? {
+        return objc_getAssociatedObject(self.base, key.unsafeMutableRawPointer()!)
     }
 
-    public func setAssociate(value: Any?, key: String)
-    {
-        objc_setAssociatedObject(self.base, key, value, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+    public func setAssociate(value: Any?, key: String) {
+        var key = key
+        objc_setAssociatedObject(self.base, &key, value, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
 
-    public func setWeakAssociate(value: Any?, key: String)
-    {
-        objc_setAssociatedObject(self.base, key, value, objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
+    public func setCopyAssociate(value: Any?, key: String) {
+        objc_setAssociatedObject(self.base, key.unsafeMutableRawPointer()!, value, objc_AssociationPolicy.OBJC_ASSOCIATION_COPY_NONATOMIC)
+    }
+    
+    public func setWeakAssociate(value: Any?, key: String) {
+        objc_setAssociatedObject(self.base, key.unsafeMutableRawPointer()!, value, objc_AssociationPolicy.OBJC_ASSOCIATION_ASSIGN)
     }
 
-    public func removeAssociates()
-    {
+    public func removeAssociates() {
         objc_removeAssociatedObjects(self.base)
     }
 }
