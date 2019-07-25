3b60bd0d5da23ba1a658479a95ac12371f28ae9d
莫锹文 <183795044@qq.com>
2019-07-25 16:48:27 +0800

    add method

--- SwiftLiteKit/Classes/Struct/Array+Extension.swift
+++ SwiftLiteKit/Classes/Struct/Array+Extension.swift
@@ -9,9 +9,37 @@ import Foundation
 
 extension Array where Element: Equatable {
     public mutating func remove(e: Element) {
-        guard self.contains(e) else { return }
+        guard contains(e) else { return }
 
         let index = self.index(of: e)!
-        self.remove(at: index)
+        remove(at: index)
+    }
+
+    public func next(for current: Element) -> Element? {
+        guard
+            let index = self.index(of: current),
+            self.count > index + 1
+        else { return nil }
+
+        return self[index + 1]
+    }
+
+    public func previous(for current: Element) -> Element? {
+        guard
+            let index = self.index(of: current),
+            index - 1 >= 0
+        else { return nil }
+
+        return self[index - 1]
+    }
+
+    public func next(for current: Element?) -> Element? {
+        guard let current = current else { return nil }
+        return next(for: current)
+    }
+
+    public func previous(for current: Element?) -> Element? {
+        guard let current = current else { return nil }
+        return previous(for: current)
     }
 }
