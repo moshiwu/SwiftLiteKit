cb00b9696a8f1b20fafaa64bbadb1a57d6b1dc64
莫锹文 <183795044@qq.com>
2019-07-30 12:26:04 +0800

    add method "resizebleCornerMaskImage"

--- SwiftLiteKit/Classes/Class/UIImage+Extension.swift
+++ SwiftLiteKit/Classes/Class/UIImage+Extension.swift
@@ -29,7 +29,7 @@ extension UIImage {
         let bundle = Bundle(identifier: bundleIdentifier)
         self.init(named: name, in: bundle, compatibleWith: traitCollection)
     }
-    
+
     /// 无缓存加载图片，带简单的文件名检测，注意这种用法每一个UIImage实例都要占用一份内存 (区别于UIImage(named:) 是使用缓存，相同名字的实例共享一份内存）
     public convenience init?(withoutCache name: String, bundle: Bundle = .main) {
         guard let path = suffix.compactMap({ bundle.path(forResource: name + $0, ofType: nil) }).first else {
@@ -46,19 +46,41 @@ extension UIImage {
     /// 中心切片拉伸
     @objc public func centerSliced() -> UIImage? {
         // 设置端盖的值
-        let top: CGFloat = self.size.height * 0.5
-        let left: CGFloat = self.size.width * 0.5
-        let bottom: CGFloat = self.size.height * 0.5
-        let right: CGFloat = self.size.width * 0.5
+        let top: CGFloat = size.height * 0.5
+        let left: CGFloat = size.width * 0.5
+        let bottom: CGFloat = size.height * 0.5
+        let right: CGFloat = size.width * 0.5
         // 设置端盖的值
         let edgeInsets: UIEdgeInsets = UIEdgeInsets(top: top, left: left, bottom: bottom, right: right)
         // 设置拉伸的模式
         let mode: UIImage.ResizingMode = .stretch
         // 拉伸图片
-        return self.resizableImage(withCapInsets: edgeInsets, resizingMode: mode)
+        return resizableImage(withCapInsets: edgeInsets, resizingMode: mode)
     }
 
     @objc public class func imageWithCenterSliced(_ name: String) -> UIImage? {
         return UIImage(named: name)?.centerSliced()
+    }
+
+    /// 创建一个圆角大小固定，可以拉伸的UIImage
+    public class func resizebleCornerMaskImage(cornerRadius: CGFloat, byRoundingCorners corners: UIRectCorner = [.allCorners], cornerColor: UIColor = .white) -> UIImage? {
+        let width = cornerRadius * 2 + 1
+        let height = cornerRadius * 2 + 1
+        
+        let rect1 = CGRect(x: 0, y: 0, width: width, height: height)
+        let rect2 = CGRect(x: -1, y: -1, width: width + 2, height: height + 2)
+        
+        UIGraphicsBeginImageContextWithOptions(rect1.size, false, 0)
+        
+        let path1 = UIBezierPath(rect: rect1)
+        cornerColor.setFill()
+        path1.fill()
+        
+        let path2 = UIBezierPath(roundedRect: rect2, byRoundingCorners: corners, cornerRadii: CGSize(width: cornerRadius, height: cornerRadius))
+        cornerColor.setFill()
+        path2.fill(with: .clear, alpha: 1)
+        
+        let capInsets = UIEdgeInsets(top: cornerRadius, left: cornerRadius, bottom: cornerRadius, right: cornerRadius)
+        return UIGraphicsGetImageFromCurrentImageContext()?.resizableImage(withCapInsets: capInsets, resizingMode: .stretch)
     }
 }
