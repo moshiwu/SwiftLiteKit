c544625e857a425237d5dd6d8a78dea1fa54965f
莫锹文 <183795044@qq.com>
2020-04-19 23:30:33 +0800

    data extension

--- SwiftLiteKit/Classes/Swift/Data+Extension.swift
+++ SwiftLiteKit/Classes/Swift/Data+Extension.swift
@@ -20,4 +20,14 @@ extension Data {
     public func subdata(in range: CountableClosedRange<Data.Index>) -> Data {
         return subdata(in: range.lowerBound ..< range.upperBound + 1)
     }
+
+    /// 将Data的二进制转成十进制的Int，最多只转换8字节（64位）的Data，超长返回-1
+    public var decimalInt: Int {
+        guard count <= 8 else { return -1 }
+        var s = 0
+        for (i, unit) in reversed().enumerated() {
+            s = s + (Int(unit) << (i * 8))
+        }
+        return s
+    }
 }
