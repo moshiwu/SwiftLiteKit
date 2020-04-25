c8115707fdd1fb002dfdf2a7b1da29cd3aa081c6
莫锹文 <183795044@qq.com>
2020-04-26 00:35:22 +0800

    add method

--- SwiftLiteKit/Classes/CoreGraphics/CGRect+Extension.swift
+++ SwiftLiteKit/Classes/CoreGraphics/CGRect+Extension.swift
@@ -19,3 +19,33 @@ public extension CGRect {
         self.init(origin: CGPoint(x: left, y: top), size: CGSize(width: right.cgFloat - left.cgFloat, height: bottom.cgFloat - top.cgFloat))
     }
 }
+
+public extension CGRect {
+    func apply(origin newValue: CGPoint) -> CGRect {
+        return CGRect(origin.x + newValue.x, origin.y + newValue.y, width, height)
+    }
+
+    func apply(size newValue: CGSize) -> CGRect {
+        return CGRect(origin.x, origin.y, width + newValue.width, height + newValue.height)
+    }
+
+    func apply(x newValue: CGFloat) -> CGRect {
+        return CGRect(origin.x + newValue, origin.y, width, height)
+    }
+
+    func apply(y newValue: CGFloat) -> CGRect {
+        return CGRect(origin.x, origin.y + newValue, width, height)
+    }
+
+    func apply(weight newValue: CGFloat) -> CGRect {
+        return CGRect(origin.x, origin.y, width + newValue, height)
+    }
+
+    func apply(height newValue: CGFloat) -> CGRect {
+        return CGRect(origin.x, origin.y, width, height + newValue)
+    }
+
+    func mult(all value: CGFloat) -> CGRect {
+        return CGRect(origin.x * value, origin.y * value, width * value, height * value)
+    }
+}
